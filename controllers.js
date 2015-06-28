bf420210bd27f78c4274b710c1e7f9f9684c5efb
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-06-28 14:06:03 -0700

    start the followers process

--- controllers.js
+++ controllers.js
@@ -33,10 +33,67 @@ app.controller('LoginCtrl', function($scope, User, $rootScope, $firebaseObject, 
     $scope.gweetObj = {};
     var currentUser = $rootScope.loggedUser.username;
     $scope.gweets = $firebaseArray($rootScope.fbGweets);
-    // var query = $rootScope.fbGweets.limitToLast(20);
-    // $scope.recentGweets = $firebaseArray(query);
-    // $scope.gweetArr = $scope.recentGweets;
-    // $scope.gweetArr = $scope.gweets;
+    $scope.followers = $firebaseArray($rootScope.fbFollowers);
+
+    var followers = $rootScope.fbRef.child('/followers');
+    var fbFollowers = $firebaseArray(followers);
+    $scope.followers = fbFollowers;
+
+
+
+    $scope.isFollowing = function(index) {
+      var query = $rootScope.fbFollowers.orderByChild("user").equalTo($rootScope.uid);
+      $scope.followArr = $firebaseArray(query);
+
+      // for (var i = 0; i < $scope.followArr.length; i++) {
+      //   console.log($scope.followArr.$keyAt(i));
+      // }
+      // console.log(query);
+
+      // var followKey = $scope.followerArr.$keyAt(index);
+      // console.log(followKey);
+      // console.log($scope.followers.$keyAt(0));
+
+      // console.log($scope.followArr[0].$id);
+
+      // var arrLength = $scope.gweets.length - 1;
+      // var realIndex = arrLength - index;
+      // var key = $scope.gweets.$keyAt(realIndex);
+      // var clickedGweet = $scope.gweets.$getRecord(key);
+      // $scope.followers.$add({
+      //   user: $rootScope.uid,
+      //   following: clickedGweet.id
+      // });
+    };
+
+    $scope.remainingChar = function() {
+      if ($scope.gweet === undefined) {
+        return 140;
+      }
+      return Gweet.maxLength - $scope.gweet.length;
+    };
+
+    $scope.isGweetMaxed = function() {
+      return $scope.remainingChar() < 0 || false;
+    };
+
+    $scope.addGweet = function() {
+      $scope.gweetObj = {};
+      $scope.gweetObj.username = currentUser;
+      $scope.gweetObj.gweet = $scope.gweet;
+      $rootScope.fbGweets = $rootScope.fbRef.child('gweets/' + $rootScope.uid);
+      $scope.gweets.$add({
+        username: currentUser,
+        gweet: $scope.gweetObj.gweet,
+        id: $rootScope.uid
+      });
+      $scope.gweet = "";
+    };
+  })
+  .controller('ProfileCtrl', function($scope, User, Gweet, $rootScope, $firebaseObject, $firebaseArray) {
+    $scope.gweetArr = [];
+    $scope.gweetObj = {};
+    var currentUser = $rootScope.loggedUser.username;
     var query = $rootScope.fbGweets.orderByChild("id").equalTo($rootScope.uid);
     $scope.gweetArr = $firebaseArray(query);
 
@@ -45,10 +102,12 @@ app.controller('LoginCtrl', function($scope, User, $rootScope, $firebaseObject, 
         return 140;
       }
       return Gweet.maxLength - $scope.gweet.length;
-    }
+    };
+
     $scope.isGweetMaxed = function() {
       return $scope.remainingChar() < 0 || false;
-    }
+    };
+
     $scope.addGweet = function() {
       $scope.gweetObj = {};
       $scope.gweetObj.username = currentUser;
@@ -60,5 +119,5 @@ app.controller('LoginCtrl', function($scope, User, $rootScope, $firebaseObject, 
         id: $rootScope.uid
       });
       $scope.gweet = "";
-    }
+    };
   });
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var app = angular.module('gwitter', ['ngRoute', 'firebase'])
   .run(function($rootScope, $firebaseAuth, $window) {
     $rootScope.fbRef = new $window.Firebase('https://twttr.firebaseio.com/');
     $rootScope.fbGweets = new $window.Firebase('https://twttr.firebaseio.com/gweets');
+    $rootScope.fbFollowers = new $window.Firebase('https://twttr.firebaseio.com/followers');
     $rootScope.afAuth = $firebaseAuth($rootScope.fbRef);
   })
   .config(function($routeProvider) {
@@ -15,6 +16,10 @@ var app = angular.module('gwitter', ['ngRoute', 'firebase'])
       .when('/home', {
         controller: 'HomeCtrl',
         templateUrl: 'home.html'
+      })
+      .when('/profile', {
+        controller: 'ProfileCtrl',
+        templateUrl: 'profile.html'
       })
       .otherwise({
         redirectTo: '/'
